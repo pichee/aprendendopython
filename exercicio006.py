58973e9055bb528be13440a17b63d449c7c30f4b
Gustavo <76635390+pichee@users.noreply.github.com>
2024-05-22 09:15:33 -0300

    Create exercicio006.py

---
+++ exercicio006.py
@@ -0,0 +1,3 @@
+#faca um programa que leia o dobro o triplo e a raiz quadrada
+n =int(input("Enter a number:\n"))
+print("the double of {} is {} the triple is {} and the squarle is {}".format(n,n*2,n*3,n*n))
