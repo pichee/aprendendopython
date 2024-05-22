7696a81ab4597afe7bfd7074ea6321990ecbcb54
Gustavo <76635390+pichee@users.noreply.github.com>
2024-05-22 09:48:37 -0300

    Create exercicio010.py

---
+++ exercicio010.py
@@ -0,0 +1,4 @@
+#faça um programa que mostre quantos dollares uma pessoa pode comprar 
+#dados que RS3,27=1dollar
+reais = float(input("How much RS do you have:"))
+print("If you have RS{}  you can buy {} dollars".format(reais,reais/3.27))
