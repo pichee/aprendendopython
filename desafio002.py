76d80163f73458e4edf31b2c1fa4a682014de3e9
Gustavo <76635390+pichee@users.noreply.github.com>
2024-05-27 15:37:50 -0300

    Delete desafio002.py

--- desafio002.py
+++
@@ -1,5 +0,0 @@
-#Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada
-dia=input('What day you born:')
-mes=input('What month you born:')
-year=input('What year you born:')
-print('You born in',dia,'/',mes,'/',year)
