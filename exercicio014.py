ee7d2d1817641fdd771a62b8f588ce343c0db30e
Gustavo <76635390+pichee@users.noreply.github.com>
2024-05-27 17:52:08 -0300

    Create exercicio014.py

---
+++ exercicio014.py
@@ -0,0 +1,5 @@
+#faça um programa que leia uma temperatura em Celsius
+#e mostre em Fahrenheit
+c = float(input("Enter a temperature in Celsius:"))
+f = c*1.8+32
+print("This is equivalent a {} Fahrenheit".format(f))
