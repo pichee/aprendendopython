bcd0547b52a1340e77b1c80a154a6ac1e4732aaf
Gustavo <76635390+pichee@users.noreply.github.com>
2024-06-28 17:19:14 -0300

    Atualizar o

---
+++ exerciciocurso0003.py
@@ -0,0 +1,8 @@
+n1=input("Enter a number:")
+n2=input("Enter other number:")
+if n1==n2:
+    print("the numbers are iguals")
+elif n1>n2:
+    print("The {} is bigger than {}".format(n1,n2))
+elif n1<n2:
+    print("The {} is bigger than {}".format(n2,n1))
