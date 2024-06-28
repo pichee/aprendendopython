db0da44e7dd769eb15808520e8470633e8e38f5f
Gustavo <76635390+pichee@users.noreply.github.com>
2024-06-28 09:59:33 -0300

    Atualizar o

---
+++ exercíciocurso0001.py
@@ -0,0 +1,12 @@
+nome="gustavo"
+sobrenome="pich"
+idade=18
+ano_de_nascimento=2006
+maior_idade="sim"
+altura=1.78
+print("Nome:",nome)
+print("Sobrenome:",sobrenome)
+print("idade:",idade)
+print("ano de nascimento:",ano_de_nascimento)
+print("é maior de idade:",maior_idade)
+print("Altura em metros:",altura)
