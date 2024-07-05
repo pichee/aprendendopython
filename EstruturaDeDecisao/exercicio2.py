197ede9d7e8a6a2b6708be4def70cf6dafea763e
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-05 20:42:56 -0300

    Create exercicio2.py

---
+++ EstruturaDeDecisao/exercicio2.py
@@ -0,0 +1,6 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo
+numero=float(int("Digite num número:"))
+if numero<0:
+    print("Seu numero é menor que negativo")
+else:
+    print("Seu numero é positivo")
