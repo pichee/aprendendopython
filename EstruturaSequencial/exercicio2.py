95806d98f77aec9b776667ddcfd0ad4e4245952e
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-04 22:33:47 -0300

    Create exercicio2.py

---
+++ EstruturaSequencial/exercicio2.py
@@ -0,0 +1,3 @@
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+numero=int(input("Diga um numero"))
+print("o numero informado foi {}".format(numero))
