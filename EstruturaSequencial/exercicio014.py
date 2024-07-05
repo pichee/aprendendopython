3da5df78abbc04e55f322e21859d14844a7eb914
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-05 12:36:12 -0300

    Atualizar o EstruturaSequencial

---
+++ EstruturaSequencial/exercicio014.py
@@ -0,0 +1,8 @@
+#João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho. Toda vez que ele traz um peso de peixes maior que o estabelecido pelo regulamento de pesca do estado de São Paulo (50 quilos) deve pagar uma multa de R$ 4,00 por quilo excedente. João precisa que você faça um programa que leia a variável peso (peso de peixes) e calcule o excesso. Gravar na variável excesso a quantidade de quilos além do limite e na variável multa o valor da multa que João deverá pagar. Imprima os dados do programa com as mensagens adequadas
+peso=float(input("Quantos kg voce pegou de peixe:"))
+if peso>50:
+  peso=peso-50
+  multa=peso*4
+  print("\nVoce pagara uma multa de {:.2f}".format(multa))
+else:
+  print("\nVoce nao pagara multa")
