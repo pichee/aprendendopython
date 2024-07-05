6a18769a6fa3c072dd21dec877d30c68134738f1
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-05 20:56:28 -0300

    Create exercicio4.py

---
+++ EstruturaDeDecisao/exercicio4.py
@@ -0,0 +1,6 @@
+#Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+letra=inputa("Digite uma letra:")
+if letra=='A' or letra=='a' or letra=='E' or letra=='e' or letra=='I' or letra=='i' or letra=='O' or letra=='o' or letra=='u' or letra=='U':
+    print("Isso é uma vogal")
+else :
+    print("Isso não é uma vogal")
