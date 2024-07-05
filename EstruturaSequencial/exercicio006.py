9a1eb97787d651d9cfdf4b31d0539f00968de3eb
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-05 10:01:11 -0300

    Atualizar o EstruturaSequencial

---
+++ EstruturaSequencial/exercicio006.py
@@ -0,0 +1,6 @@
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+raio=float(input("Digite o raio:"))
+raio=raio*raio
+pi=float(3.14)
+area=pi*raio
+print("A area do seu circulo equivale a {:.2f} metros ao quadrado".format(area))
