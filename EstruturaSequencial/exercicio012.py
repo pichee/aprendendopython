056569701a8eb44d348e6e9a107bd811ea1de911
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-05 11:47:20 -0300

    Atualizar o EstruturaSequencial

---
+++ EstruturaSequencial/exercicio012.py
@@ -0,0 +1,4 @@
+#Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
+altura=float(input("Qual sua altura"))
+ideal=72.7*altura-58
+print("Com a altura {} o peso ideal é {:.2f}".format(Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58,ideal))
