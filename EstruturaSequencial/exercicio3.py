cf2330c0d1c58b18e5e3dc1c99a4af6bfd3de68e
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-04 22:37:00 -0300

    Create exercicio3.py

---
+++ EstruturaSequencial/exercicio3.py
@@ -0,0 +1,5 @@
+#Faça um Programa que peça dois números e imprima a soma.
+numero1=float(input("Digite um numero:"))
+numero2=float(input("Digite outro  numero:"))
+numero3=numero1+numero2
+print("A Soma ente {} + {} é {}".format(numero1,numero2,numero3))
