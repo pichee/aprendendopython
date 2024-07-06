929feacfdc4e07188c0e276c481413ee1ecc1003
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-06 17:17:26 -0300

    Atualizar o EstruturaDeDecisao

---
+++ EstruturaDeDecisao/exercicio007.py
@@ -0,0 +1,21 @@
+#Faça um Programa que leia três números e mostre o maior e o menor deles.
+numero1=float(input("Digite um numero:"))
+numero2=float(input("Digite um numero:"))
+numero3=float(input("Digite um numero:"))
+if numero1==numero2 or numero1==numero3 or numero3==numero2:
+    print("Não coloque números iguais")
+if numero2>numero1 and numero2>numero3 and numero1<numero3:
+    print("O numero {} é maior e o menor é {}".format(numero2,numero1))
+if numero2>numero1 and numero2>numero3 and numero3<numero1:
+    print("O numero {} é maior e o menor é {}".format(numero2,numero3))
+if numero3>numero1 and numero3>numero2 and numero1<numero2:
+    print("O numero {} é maior e o menor é {}".format(numero3,numero1))
+if numero3>numero1 and numero3>numero2 and numero2<numero1:
+    print("O numero {} é maior e o menor é {}".format(numero3,numero1))
+if numero1>numero3 and numero1>numero2 and numero2<numero3:
+    print("O numero {} é maior e o menor é {}".format(numero1,numero2))
+if numero1>numero3 and numero1>numero2 and numero3<numero2:
+    print("O numero {} é maior e o menor é {}".format(numero1,numero3))
+    
+    
+    
