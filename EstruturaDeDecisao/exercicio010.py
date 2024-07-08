570fff18ab1524c1cb11fdee2b691513fa26fba7
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-08 09:13:06 -0300

    Atualizar o EstruturaDeDecisao

---
+++ EstruturaDeDecisao/exercicio010.py
@@ -0,0 +1,10 @@
+#Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino ou N- Noturno. Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+turno=input("Em que turno voce estuda\nmanha[m]\nvespertino[v]\nNoturno[n]\n")
+if turno=='m':
+  print("Bom Dia!")
+elif turno=='v':
+  print("Boa Tarde!")
+elif turno=='n':
+  print("Boa Noite")
+else:
+  print("Error")
