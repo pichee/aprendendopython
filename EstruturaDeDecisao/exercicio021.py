bdd13432d7166d53f0f312d53d014b9685a62df1
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-10 12:29:37 -0300

    Atualizar o exercicio021.py

--- EstruturaDeDecisao/exercicio021.py
+++ EstruturaDeDecisao/exercicio021.py
@@ -1,17 +1,21 @@
-#Faça um Programa para leitura de três notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e presentar:
-#A mensagem "Aprovado", se a média for maior ou igual a 7, com a respectiva média alcançada;
-#A mensagem "Reprovado", se a média for menor do que 7, com a respectiva média alcançada;
-#A mensagem "Aprovado com Distinção", se a média for igual a 10.
-
-nota1=float(input("Digite sua primeira nota:"))
-nota2=float(input("Digite sua segunda nota:"))
-nota3=float(input("Digite sua terceira  nota:"))
-media=(nota1+nota2+nota3)/3
-if media>10:
- print("Error")
-elif media<7:
- print("Reprovado")
-elif media==10:
- print("Aprovado com distinção")
-else:
- print("Aprovado")
+#Faça um Programa para um caixa eletrônico. O programa deverá perguntar ao usuário a valor do saque e depois informar quantas notas de cada valor serão fornecidas. As notas disponíveis serão as de 1, 5, 10, 50 e 100 reais. O valor mínimo é de 10 reais e o máximo de 600 reais. O programa não deve se preocupar com a quantidade de notas existentes na máquina.
+#Exemplo 1: Para sacar a quantia de 256 reais, o programa fornece duas notas de 100, uma nota de 50, uma nota de 5 e uma nota de 1;
+#Exemplo 2: Para sacar a quantia de 399 reais, o programa fornece três notas de 100, uma nota de 50, quatro notas de 10, uma nota de 5 e quatro notas de 1.
+quantidade=int(input("Digite a a quantidade que deseja sacar[minimo 10 maximo 600]"))
+total=quantidade
+centena=quantidade//100
+quantidade=quantidade%100
+cinquenta=quantidade//50
+quantidade=quantidade%50
+dezena=quantidade//10
+quantidade=quantidade%10
+cinco=quantidade//5
+quantidade=quantidade%5
+unidade=quantidade
+if total>600:
+  print("\nValor maior que o limite")
+  exit()
+if total<10:
+  print("\nValor menor que o limite")
+  exit()
+print("\nPara sacar a quantia de {} reais,o programa fornecera {} notas de 100,{} notas de 10,{} notas de 5,{} moedas de 1 real".format(total,centena,cinquenta,dezena,cinco,unidade))
