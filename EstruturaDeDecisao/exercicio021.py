e4abed1c95c117e49a05d2d3443846299dacee44
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-10 10:55:33 -0300

    Atualizar o EstruturaDeDecisao

---
+++ EstruturaDeDecisao/exercicio021.py
@@ -0,0 +1,17 @@
+#Faça um Programa para leitura de três notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e presentar:
+#A mensagem "Aprovado", se a média for maior ou igual a 7, com a respectiva média alcançada;
+#A mensagem "Reprovado", se a média for menor do que 7, com a respectiva média alcançada;
+#A mensagem "Aprovado com Distinção", se a média for igual a 10.
+
+nota1=float(input("Digite sua primeira nota:"))
+nota2=float(input("Digite sua segunda nota:"))
+nota3=float(input("Digite sua terceira  nota:"))
+media=(nota1+nota2+nota3)/3
+if media>10:
+ print("Error")
+elif media<7:
+ print("Reprovado")
+elif media==10:
+ print("Aprovado com distinção")
+else:
+ print("Aprovado")
