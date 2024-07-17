c91c624a0dbf6e9cf7143b2f5dbf0206ff006e32
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-17 09:53:54 -0300

    Atualizar o EstruturaDeRepeticao

---
+++ EstruturaDeRepeticao/exercicio024.py
@@ -0,0 +1,10 @@
+#Faça um programa que calcule o mostre a média aritmética de N notas.
+quanto=int(input("Quantas notas voce quer calcular:"))
+media=0
+aux=1
+for aux in range(quanto):
+  nota=int(input("Qual foi sua nota:"))
+  media=media+nota
+
+media=media/quanto
+print("A media dessas notas é {}".format(media))
