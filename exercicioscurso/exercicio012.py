e30e9d9b58e74a998823cd3f9a4f63f72b1e3ad0
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-21 09:54:33 -0300

    Delete exercicioscurso/exercicio012.py

--- exercicioscurso/exercicio012.py
+++
@@ -1,5 +0,0 @@
-#faça um algoritmo que leia o preço de um produto e mostre seu novo preço com
-#5% de desconto
-n = float(input("Enter a price this product:"))
-d = n-(n*0.05)
-print("If the product receb a discont the %5 the new value is {}".format(d))
