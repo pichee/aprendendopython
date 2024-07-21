d1a13428921658293e17a372d27532f1e1992cbe
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-21 09:53:39 -0300

    Delete exercicioscurso/exercicio008.py

--- exercicioscurso/exercicio008.py
+++
@@ -1,6 +0,0 @@
-#escreva um programa que leia um valor em metros
-#e o exiba convertido em centimetros e milimetros
-m = float(input("Enter a value in meters:"))
-c = m*100
-mi = m*1000
-print("The {} in cm is igual {} and in mm is {} igusl".format(m,c,mi))
