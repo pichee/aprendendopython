807a6a31d520efc762e0bcfd25994ba72628a0a5
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-21 09:53:23 -0300

    Delete exercicioscurso/exercicio007.py

--- exercicioscurso/exercicio007.py
+++
@@ -1,7 +0,0 @@
-#desenvolva um programa que leia as duas notas
-#de um aluno,calcule e mostre a sua media
-
-n1 =int(input("Enter the first note:"))
-n2 =int(input("Enter the second note:"))
-n3 = (n1+n2)/2
-print("The media {} and {} is {}".format(n1,n2,n3))
