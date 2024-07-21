20903f47eb53447798ff5fd007f77c7c531b635e
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-21 09:55:25 -0300

    Delete exercicioscurso/exercicio016.py

--- exercicioscurso/exercicio016.py
+++
@@ -1,4 +0,0 @@
-# Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira
-import math
-n= float(input("Enter a number:"))
-print("The value is {} but your part int is {:.0f}".format(n,math.trunc(n)))
