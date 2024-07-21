bfa20add2081badfb00e6ee1e99bace728e06b10
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-21 09:53:07 -0300

    Delete exercicioscurso/exercicio006.py

--- exercicioscurso/exercicio006.py
+++
@@ -1,3 +0,0 @@
-#faca um programa que leia o dobro o triplo e a raiz quadrada
-n =int(input("Enter a number:\n"))
-print("the double of {} is {} the triple is {} and the squarle is {}".format(n,n*2,n*3,n*n))
