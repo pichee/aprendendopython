053d177d3c23151ad0977674e7014d06ff2d1b00
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-21 09:55:59 -0300

    Delete exercicioscurso/exercicio017.py

--- exercicioscurso/exercicio017.py
+++
@@ -1,7 +0,0 @@
-# Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo. Calcule e mostre o comprimento da hipotenusa.
-import math
-co=float(input("Enter the size the cateto oposto:"))
-ca=float(input("Enter the size the cateto adjacente:"))
-h=(co*co)+(ca*ca)
-h=math.sqrt(h)
-print("The hipotenusa is igual {:.2f}".format(h))
