e169bf54975009422fa0ac0aed46bf6cad226429
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-21 09:54:44 -0300

    Delete exercicioscurso/exercicio013.py

--- exercicioscurso/exercicio013.py
+++
@@ -1,5 +0,0 @@
-#faça um algoritmo que leia o salario de um funcionario
-#e mostre seu novo salario com 15% de aumento
-salary=float(input("Enter a salary:"))
-aum = salary+(salary*0.15)
-print("if this salary {} have a increase thee neew value is {}".format(salary,aum))
