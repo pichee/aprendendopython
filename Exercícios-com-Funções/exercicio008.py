5a4ca925adb3e03996525f3eb6157bbd1992d456
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-29 09:28:40 -0300

    Atualizar o Exercícios-com-Funções

---
+++ Exercícios-com-Funções/exercicio008.py
@@ -0,0 +1,7 @@
+#Faça uma função que informe a quantidade 
+#de dígitos de um determinado número inteiro informado.
+def numero(x):
+  numero=input("Insera um valor:")
+  contador=len(numero)
+  print(f"A quantidade de digitos desse numero é {contador}")
+numero(0)
