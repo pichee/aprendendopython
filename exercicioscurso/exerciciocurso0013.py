948cea7d7241ef6b8478a70fefe971b0a53d9cfd
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-30 16:26:44 -0300

    Atualizar o exercicioscurso

---
+++ exercicioscurso/exerciciocurso0013.py
@@ -0,0 +1,9 @@
+def multiplicacao(x):
+  numero=0
+    numero=int(input("Digite um numero:"))
+    if numero%2==0:
+      print("Esse numero é par")
+    else:
+      print("Esse numero é impar")
+      
+def multiplicacao(0)
