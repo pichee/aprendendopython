8bfdac69ebcf6a85fafa70547bce985c1d0912cb
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-02 08:59:18 -0300

    Atualizar o exercicioscurso

---
+++ exercicioscurso/exerciciocurso0014.py
@@ -0,0 +1,8 @@
+def multiplicao(x):
+    numero=int(input("Digite um numero"))
+    numeroaoquadrado=numero*2
+    numeroaocubo=numero*3
+    numeroxquatro=numero*4
+    print(f"Esse numero*2={numeroaoquadrado}, *3={numeroaocubo}, *4={numeroxquatro}")
+    
+multiplicao(0)
